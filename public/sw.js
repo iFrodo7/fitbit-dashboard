59e1b2abdffaee4ff20143dd80b2be394a8fa92f
Diego Castillo <dcv@192.168.1.13>
2026-06-08 19:26:25 -0600

    chore: bump SW a v46 para forzar re-cache tras rollback

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'fitbit-air-v45';
+const CACHE_VERSION = 'fitbit-air-v46';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
